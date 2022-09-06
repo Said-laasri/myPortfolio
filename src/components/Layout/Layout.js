aae8289f314044337fb1e10d4b70bd35093fa87e
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-09-06 16:51:59 +0100

    add home component

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import { Outlet } from 'react-router-dom';
 import Sidebar from '../Sidebar/Sidebar';
 import './Layout.scss';
 
 function Layout() {
   return (
-    <>
+    <div className="App">
       <Sidebar />
-      <h1>fff</h1>
-    </>
+      <div className="page">
+        <span className="tags top-tags">&lt;body&gt;</span>
+
+        <Outlet />
+
+        <span className="tags bottom-tags">
+          &lt;body&gt;
+          <br />
+          <span className="bottom-tag-html">&lt;html&gt;</span>
+        </span>
+      </div>
+    </div>
   );
 }
 
