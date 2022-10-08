899a1db78ba23c8bb4cdbb10de59b6bd0cfd53f6
Said-laasri <100702612+Said-laasri@users.noreply.github.com>
2022-10-08 12:36:51 +0100

    Add project file and function

---
+++ src/components/Portfolio/projects/index.js
@@ -0,0 +1,25 @@
+/* eslint-disable react/button-has-type */
+function Project() {
+  return (
+    <div className="cards-project">
+      <div className="card-f" />
+      <img src=" " alt=" " />
+      <h2 />
+      <p />
+      <div>
+        <button className="github-source">
+          <a href=" " target="_blank" rel="noreferrer">
+            Github
+          </a>
+        </button>
+        <button className="live_link">
+          <a href=" " target="_blank" rel="noreferrer">
+            Live
+          </a>
+        </button>
+      </div>
+    </div>
+  );
+}
+
+export default Project;
